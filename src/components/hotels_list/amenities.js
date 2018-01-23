edeb2f16d9298267b857a6c7ed07a0bb9d027c49
maacaro <maacaro@gmail.com>
2018-01-23 05:52:59 -0300

    master remove unused var

--- src/components/hotels_list/amenities.js
+++ src/components/hotels_list/amenities.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const assetesIconsPath = '../../assets/icons/amenities/'
-
 const Amenities =({amenities})=>
 <ul style={{float:'left'}}>{amenities.map((amenity,key) => <li key={key} style={{display:'inline', padding: '6px'}}>
     <img src={require(`../../assets/icons/amenities/${amenity}.svg`)} height="18" width="18" />
