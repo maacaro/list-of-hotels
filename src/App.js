038a290b05ebb2a9691d770eeb2e361f8f56ec7e
maacaro <maacaro@gmail.com>
2018-01-23 19:44:50 -0300

    master Reder list of hotel an search area

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import SearchName from './components/search/searchName';
-import logo from './logo.svg';
+import Search from './components/search/search';
+import Hotel from './components/hotels_list/hotel';
+import getHotels from './Api-Services/hotelsApi';
 import './App.css';
 
-const amenities= [
-  'coffe-maker',
-  'restaurant',
-  'bathtub',
-  'beach-pool-facilities',
-  'separate-bredroom'
-];
+const HotelList = ({hotels})=> 
+<div className = 'hotel-container'>
+  {hotels.map(hotel =><Hotel key={hotel.id} name ={hotel.name} stars={hotel.stars} price={hotel.price} amenities={hotel.amenities} image={hotel.image}/> )}
+</div>
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotels:[],
+      nameSearch:'',
+      starSearch:'ALL'
+    }
+  }
+
+  componentWillMount = async ()=>{
+    const hotels =  await getHotels(this.state.nameSearch, this.state.starSearch);
+    this.setState(
+      {
+        hotels
+      });
+  }
+
   render() {
     return (
-      <SearchName/>
+      <div className='container'>
+        <Search/>
+        <HotelList hotels={this.state.hotels}/>
+      </div>
     );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import getHotels from './Api-Services/hotelsApi';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const data = [{
+  id: "249942",
+  name: "Hotel Stefanos",
+  stars: 3,
+  price: 994.18,
+  image: "4900059_30_b.jpg",
+  amenities: [
+    "safety-box",
+    "nightclub",
+    "deep-soaking-bathtub",
+    "beach",
+    "business-center"
+  ]
+},
+{
+  id: "161901",
+  name: "Hotel Santa Cruz",
+  stars: 3,
+  price: 1267.57,
+  image: "6623490_6_b.jpg",
+  amenities: [
+    "nightclub",
+    "business-center",
+    "bathtub",
+    "newspaper",
+    "restaurant"
+  ]
+}];
+
+global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, status:200, json: () => data }));
+
+it('should render the search area',()=>{
+  const app = shallow(<App />);
+  expect(app.find('Search')).toHaveLength(1);
+})
+
+it('should render all the hotels returned in the fetch call',()=>{
+  global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, status:200, json: () => data }));
+  
+   const app = shallow(<App />);
+
+  expect(app.find('HotelList')).toHaveLength(1);
 });
+
+
