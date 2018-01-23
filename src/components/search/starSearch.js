805b4ffa0a8dc54c2effc86d9fa9366520686ff4
maacaro <maacaro@gmail.com>
2018-01-23 20:41:21 -0300

    master Handle click on stars search

--- src/components/search/starSearch.js
+++ src/components/search/starSearch.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Stars from '../stars/stars';
 
-const StarSearch = ()=>
+const StarSearch = ({onClick})=>
 <div>
     <div><label>Estrellas</label></div>
-    <input type="checkbox" name="stars" value="ALL" defaultChecked />Todas las estrellas<br/>
-    <input type="checkbox" name="stars" value="5" /><Stars value="5"/><br/>
-    <input type="checkbox" name="stars" value="4"/><Stars value="4"/><br/>
-    <input type="checkbox" name="stars" value="3" /><Stars value="3"/><br/>
-    <input type="checkbox" name="stars" value="2"/><Stars value="2"/><br/>
-    <input type="checkbox" name="stars" value="1"/><Stars value="1"/><br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick('ALL')} value="ALL" defaultChecked />Todas las estrellas<br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick(5)} value="5" /><Stars value="5"/><br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick(4)} value="4"/><Stars value="4"/><br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick(3)} value="3" /><Stars value="3"/><br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick(2)} value="2"/><Stars value="2"/><br/>
+    <input type="checkbox" name="stars" onClick ={()=>onClick(1)} value="1"/><Stars value="1"/><br/>
 </div>
 
 export default StarSearch;
--- src/components/search/starsSearch.test.js
+++ src/components/search/starsSearch.test.js
@@ -14,3 +14,59 @@ it('shuld render a label',()=>{
     
     expect(starSearch.find('label')).toHaveLength(1); 
 });
+
+describe('the behavior',()=>{
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(0).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith('ALL');
+    });
+
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(1).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith(5);
+    });
+
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(2).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith(4);
+    });
+
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(3).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith(3);
+    });
+
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(4).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith(2);
+    });
+
+    it('should call the callback funcion on click',()=>{
+        const callback = jest.fn();
+        const starSearch = shallow(<StarSearch onClick={callback}/>);
+        
+        starSearch.find('input').at(5).simulate('click');
+
+        expect(callback).toHaveBeenCalledWith(1);
+    });
+});
